08acd7c9fbbc8e2568e4b8d2018d68814af2e1fa
VladimirFomin90 <vladimirfomin90@yandex.ru>
2023-08-09 23:19:59 +0600

    Tailwind CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,20 +4,22 @@
 
 @section('content')
 
-    <div>
-        <a href="{{ route('tasks.create') }}">Add task</a>
-    </div>
+    <button class="my-4 font-bold">
+        <a href="{{ route('tasks.create') }} "
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add task</a>
+    </button>
 
     @forelse ( $tasks as $task)
         <div>
-            <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a> <br />
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}"
+                @class(['line-through' => $task->completed, 'font-bold'])>{{ $task->title }}</a> <br />
         </div>
     @empty
         <div>There are no tasks!</div>
     @endforelse
 
     @if ($tasks->count())
-        <nav>
+        <nav class="my-3">
             {{ $tasks->links() }}
         </nav>
     @endif
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,18 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Laravel 10 Task List App</title>
     @yield('styles')
 </head>
-<body>
-    <h1>@yield('title')</h1>
+<body class="container mx-auto my-10 max-w-xl">
+    <h1 class="font-bold text-2xl">@yield('title')</h1>
     <div>
         @if (session()->has('success'))
       <div>{{ session('success') }}</div>
         @endif
-        
+
         @yield('content')
     </div>
-    
+
 </body>
 </html>
